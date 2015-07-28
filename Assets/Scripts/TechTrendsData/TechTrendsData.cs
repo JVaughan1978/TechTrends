3a4970b8ad645df79869156217a554e3af2d8521
JoshuaVaughan <josh@bravomediainc.com>
2015-07-28 14:56:43 -0400

    Small Tweaks to Wrapper and Data

--- Assets/Scripts/TechTrendsData/TechTrendsData.cs
+++ Assets/Scripts/TechTrendsData/TechTrendsData.cs
@@ -53,17 +53,13 @@ public class TechTrendsData {
 	}
 
 	public static TechTrendsData Load(string path){
-		//TextAsset textAsset = (TextAsset) Resources.Load("TechTrends_Data-23-07-2015");  
-		TextAsset textAsset = (TextAsset) Resources.Load(path);  
-		XmlDocument xmldoc = new XmlDocument ();
-		xmldoc.LoadXml ( textAsset.text );
+		XmlDocument xmldoc = new XmlDocument();
+		TextAsset textAsset = (TextAsset)Resources.Load (path);
+		xmldoc.LoadXml (textAsset.text);
 
-		XmlSerializer serializer = new XmlSerializer(typeof(TechTrendsData));
-		/*using(FileStream stream = new FileStream(path, FileMode.Open)) {
-			return serializer.Deserialize(stream) as TechTrendsData;
-		}Ye olden windowed way...*/
-
+		XmlSerializer serializer = new XmlSerializer(typeof(TechTrendsData));		
 		using (XmlReader reader = new XmlNodeReader(xmldoc)) {
+			Debug.Log ("Reading data from " + path);
 			return serializer.Deserialize(reader) as TechTrendsData;
 		}
 	}
