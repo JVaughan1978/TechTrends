79e1ac5532257b63d4d54451485bd0a7f392973a
JoshuaVaughan <josh@bravomediainc.com>
2015-07-29 12:25:32 -0400

    Tweak to highlight
    Actually working properly now

--- Assets/Scripts/Animation/Highlight.cs
+++ Assets/Scripts/Animation/Highlight.cs
@@ -6,11 +6,12 @@ public class Highlight : MonoBehaviour {
 	public float zOffset = -0.25f;
 	public Material highlightMaterial;
 
+    private bool _animating = false;
 	private bool _highlighted = false;
 	private bool _lasthighlight = false;
 	private float _duration = 0.5f;
 	private float _time = 0;
-    private bool _positionSet = true;
+    private bool _positionSet = false;
 	private Vector3 _originalPos = Vector3.zero;
 	private Vector3 _highlightedPos = Vector3.zero;
 	private Material _originalMaterial;
@@ -28,6 +29,7 @@ public class Highlight : MonoBehaviour {
 	void Highlighted(string broadcastName) {
 		if (gameObject.name == broadcastName) {
             _highlighted = true;
+            _animating = true;
 		}
 	}
 
@@ -57,11 +59,11 @@ public class Highlight : MonoBehaviour {
 	void Update () {
         //material swapping
         if(_highlighted != _lasthighlight) { 
-            _positionSet = false;
+           // _positionSet = false;
         }
         
-        if(!_positionSet) {
-            if (CompareVectors(transform.localPosition, _originalPos, 0.1f){
+        if(_animating && !_positionSet) {
+            if (CompareVectors(transform.localPosition, _originalPos, 0.1f)){
                 _positionSet = true;
             } else {
                 _originalPos = transform.localPosition;
@@ -70,17 +72,18 @@ public class Highlight : MonoBehaviour {
             }
         }
 
-        if (_highlighted) {
-           
-            _time += Time.deltaTime;
-            _time = Mathf.Clamp(_time, 0f, _duration);
-            float lerp = Easing.ExpoEaseInOut(_time, 0f, 1f, _duration);
-            transform.localPosition = Vector3.Lerp(_originalPos, _highlightedPos, lerp);
-        } else {
-            _time -= Time.deltaTime;
-            _time = Mathf.Clamp(_time, 0f, _duration);
-            float lerp = Easing.ExpoEaseInOut(_time, 0f, 1f, _duration);
-            transform.localPosition = Vector3.Lerp(_originalPos, _highlightedPos, lerp);
+        if(_animating) { 
+            if (_highlighted) {           
+                _time += Time.deltaTime;
+                _time = Mathf.Clamp(_time, 0f, _duration);
+                float lerp = Easing.ExpoEaseInOut(_time, 0f, 1f, _duration);
+                transform.localPosition = Vector3.Lerp(_originalPos, _highlightedPos, lerp);
+            } else {
+                _time -= Time.deltaTime;
+                _time = Mathf.Clamp(_time, 0f, _duration);
+                float lerp = Easing.ExpoEaseInOut(_time, 0f, 1f, _duration);
+                transform.localPosition = Vector3.Lerp(_originalPos, _highlightedPos, lerp);
+            }
         }
 
         _lasthighlight = _highlighted;
