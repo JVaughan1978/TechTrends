ee3c352568462f79852442d3041dd86f9c8b4804
JoshuaVaughan <josh@bravomediainc.com>
2015-08-07 12:11:34 -0400

    Pulsing 3D Graph

---
+++ Assets/Scripts/Animation/GraphPulse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphPulse : MonoBehaviour {
+
+    public float duration = 6.0f;
+    	
+    void Update() {
+        
+        Vector3 firstPos = new Vector3(1f, 1f, 1f);
+        Vector3 lastPos = new Vector3(1.5f, 1f, 1f);
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+
+        transform.localScale = Vector3.Lerp(firstPos, lastPos, lerp);
+    }
+}
