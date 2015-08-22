4f57497e5815724af706447446ce4dca15b0676b
JoshuaVaughan <josh@bravomediainc.com>
2015-08-21 21:49:12 -0400

    Adding thingamabobs

---
+++ Assets/Scripts/QualityChooser.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QualityChooser : MonoBehaviour {
+    
+    void Start() {
+
+#if UNITY_EDITOR
+    Debug.Log("Unity Editor");
+    QualitySettings.SetQualityLevel(0, true);
+#endif
+
+#if UNITY_IPHONE
+    Debug.Log("Iphone");           
+    QualitySettings.SetQualityLevel(1, false);
+#endif
+
+#if UNITY_ANDROID
+    Debug.Log("Android");
+    QualitySettings.SetQualityLevel(0, false);
+#endif
+    }
+}
