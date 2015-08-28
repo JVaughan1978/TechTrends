ebb708095911d2cec3b40ddb43ba57a1722e6cae
JoshuaVaughan <josh@bravomediainc.com>
2015-08-28 19:17:04 -0400

    Much Animating

---
+++ Assets/Scripts/Animation/LogoShove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoShove : MonoBehaviour {
+        
+    public float speed = 0.3f;    
+    public float offset = 1.0f;
+
+    private Vector3 _initialPosition;
+    private float _currentTime = 0;
+    private bool _move = false;
+
+    void OnEnable() {
+        DelayedRun.OnEnd += Shove;
+    }
+
+    void OnDisable() {
+        DelayedRun.OnEnd -= Shove;
+    }
+
+    void Shove() {
+        _move = true;
+    }
+
+	// Use this for initialization
+	void Start () {
+        _initialPosition = transform.localPosition;        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(_move) {
+            _currentTime += Time.deltaTime;
+        }
+
+        if(_currentTime < speed) {
+            float currentOffset = offset * ((speed - _currentTime) / speed);
+            Vector3 currentPosition = new Vector3(_initialPosition.x, _initialPosition.y - currentOffset, _initialPosition.z);
+            transform.localPosition = currentPosition;
+        }	
+	}
+}
