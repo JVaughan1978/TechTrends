e1660c4301232d84d09263fe1c8d80cacde9d4e2
JVaughan1978 <Joshua.S.Vaughan@gmail.com>
2015-09-02 18:45:45 -0400

    Moving the ol' puck forward

---
+++ Assets/Scripts/DeScaler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeScaler : MonoBehaviour {
+
+    public GameObject scaleRoot;
+    public float scaler;
+
+	void Update () {
+	    float xScale = (1.0f / scaleRoot.transform.localScale.x  ) * scaler;
+        transform.localScale = new Vector3(xScale, 1.0f, 1.0f);
+	}
+}
---
+++ Assets/Scripts/PinToPosition.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinToPosition : MonoBehaviour {
+
+    public GameObject target;
+
+	void Update () {
+        transform.position = target.transform.position;
+	}
+}
